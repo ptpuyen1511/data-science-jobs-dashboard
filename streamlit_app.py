9016b59c4f1ff5eeaf8c11ea5056ff120e589a04
ptpuyen1511 <ptpuyen1511@gmail.com>
2024-02-22 15:01:32 +0700

    update: changing layout with boarder for containers

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,12 +76,12 @@ def make_barh_skills_chart(jobs_df):
 
 # Dashboard Main Panel
 st.markdown('<h2 style="text-align: center; color: black; background-color: #9ec3ff;">US Data Science Jobs Dashboard</h2>', unsafe_allow_html=True)
+st.write('')
 
 row1_col = st.columns(2)
 
 with row1_col[0]:
     container = st.container(border=True)
-    container.write('')
     container.markdown('<h4 style="text-align: center; color: black; background-color: #9ec3ff;">Jobs by Location</h4>', unsafe_allow_html=True)
     # st.markdown('#### Jobs by Location')
     symbol_map_fig = make_symbol_map(jobs_df, long_lat_df, two_letters_state_df)
@@ -89,31 +89,35 @@ with row1_col[0]:
     plt.figure()
 
 with row1_col[1]:
-    st.markdown('<h4 style="text-align: center; color: black; background-color: #9ec3ff;">Jobs Table</h4>', unsafe_allow_html=True)
+    container = st.container(border=True)
+    container.markdown('<h4 style="text-align: center; color: black; background-color: #9ec3ff;">Jobs Table</h4>', unsafe_allow_html=True)
     # st.markdown('#### Jobs Table')
 
 
 row2_col = st.columns(3) # 3 columns with equal width
 
 with row2_col[0]:
-    st.markdown('<h4 style="text-align: center; color: black; background-color: #9ec3ff;">Jobs by Level</h4>', unsafe_allow_html=True)
+    container = st.container(border=True)
+    container.markdown('<h4 style="text-align: center; color: black; background-color: #9ec3ff;">Jobs by Level</h4>', unsafe_allow_html=True)
     # st.markdown('#### Jobs by Level')
     barh_chart_ax = make_barh_chart(jobs_df)
-    st.pyplot(barh_chart_ax.figure, use_container_width=True)
+    container.pyplot(barh_chart_ax.figure, use_container_width=True)
     plt.figure()
 
 with row2_col[1]:
-    st.markdown('<h4 style="text-align: center; color: black; background-color: #9ec3ff;">Top 20 Needed Skills</h4>', unsafe_allow_html=True)
+    container = st.container(border=True)
+    container.markdown('<h4 style="text-align: center; color: black; background-color: #9ec3ff;">Top 20 Needed Skills</h4>', unsafe_allow_html=True)
     # st.markdown('#### Top 20 Needed Skills')
     wc_chart_plt, top_5 = make_wordcloud_chart(jobs_df)
-    st.markdown(f'**Top 5 skills: {", ".join(top_5)}**')
-    st.pyplot(fig=wc_chart_plt, use_container_width=True)
+    container.markdown(f'**Top 5 skills: {", ".join(top_5)}**')
+    container.pyplot(fig=wc_chart_plt, use_container_width=True)
     plt.figure()
     
 
 with row2_col[2]:
-    st.markdown('<h4 style="text-align: center; color: black; background-color: #9ec3ff;">Top 5 Skills by Level*</h4>', unsafe_allow_html=True)
+    container = st.container(border=True)
+    container.markdown('<h4 style="text-align: center; color: black; background-color: #9ec3ff;">Top 5 Skills by Level*</h4>', unsafe_allow_html=True)
     # st.markdown('#### Top 5 Skills by Level*')
     barh_skills_chart_plt = make_barh_skills_chart(jobs_df)
-    st.markdown('**After removing top 5 skills from top 20*')
-    st.pyplot(fig=barh_skills_chart_plt, use_container_width=True)
+    container.markdown('**After removing top 5 skills from top 20*')
+    container.pyplot(fig=barh_skills_chart_plt, use_container_width=True)
