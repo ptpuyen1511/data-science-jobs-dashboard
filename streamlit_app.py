bd4b4b8e1cba27b916e61fb851347deb1861d9d3
ptpuyen1511 <ptpuyen1511@gmail.com>
2024-02-22 09:40:55 +0700

    update: adding wordcloud chart

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 
 # Page configuration
 st.set_page_config(page_title="US Data Science Jobs Dashboard", page_icon=":bar_chart:", layout="wide")
@@ -23,15 +24,29 @@ def make_barh_chart(jobs_df):
     ax.set_xlabel('Count')
     ax.set_ylabel('Job Level')
     ax.bar_label(ax.containers[0])
+    ax.set_frame_on(False)
 
     return ax
 
 ## WordCloud (top 20 needed skills) and Top 5 words (top 5 needed skills)
+def make_wordcloud_chart(jobs_df):
+    skill_counts = pd.DataFrame(jobs_df['job_skills'].str.split(', ', expand=False).values.tolist()).stack().value_counts()
+    skills_freq = skill_counts.head(20)
+
+    all_wc = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(skills_freq)
+    plt.axis('off')
+    plt.imshow(all_wc, interpolation='bilinear')
+    ax = plt.gcf()
+
+    return ax
+
 
 ##  Barh charts (count skills by levels)
 
 # Dashboard Main Panel
 col = st.columns(2) # Two columns with equal width
+
+st.markdown('## US Data Science Jobs Dashboard')
 
 with col[0]:
     st.markdown('### Jobs by Location')
@@ -43,5 +58,7 @@ with col[0]:
 
 with col[1]:
     st.markdown('### Top 20 Needed Skills')
+    wc_chart = make_wordcloud_chart(jobs_df)
+    st.pyplot(wc_chart.figure)
 
     st.markdown('### Top 5 Need Skills by Level')
