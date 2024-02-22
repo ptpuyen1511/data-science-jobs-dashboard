d817d7d31d728d1d8bde64b377d7682f0af43a0e
ptpuyen1511 <ptpuyen1511@gmail.com>
2024-02-22 08:52:59 +0700

    add: building streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,49 @@
+# Inspired from a tutorial "Building a dashboard in Python using Streamlit" (By Chanin Nantasenamat)
+
+# Import libraries
+import streamlit as st
+import altair as alt
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+# Page configuration
+st.set_page_config(page_title="US Data Science Jobs Dashboard", page_icon=":bar_chart:", layout="wide")
+alt.themes.enable('opaque')
+
+# Load preprocessed dataset
+jobs_df = pd.read_csv('data/preprocessed_jobs.csv')
+
+# Plots
+## Symbol map (count jobs by location)
+
+## Barh chart (count jobs by level)
+def make_barh_chart(jobs_df):
+    count_jobs_df = pd.DataFrame(jobs_df.groupby(['job_level']).size().sort_values(ascending=True)).reset_index().rename(columns={0: 'count'}).set_index('job_level')
+    
+    ax = count_jobs_df.plot.barh(title='', legend=False)
+    ax.set_xlabel('Count')
+    ax.set_ylabel('Job Level')
+    ax.bar_label(ax.containers[0])
+
+    return ax
+
+## WordCloud (top 20 needed skills) and Top 5 words (top 5 needed skills)
+
+##  Barh charts (count skills by levels)
+
+# Dashboard Main Panel
+col = st.columns(2) # Two columns with equal width
+
+with col[0]:
+    st.markdown('### Jobs by Location')
+    
+
+    st.markdown('### Jobs by Level')
+    barh_chart = make_barh_chart(jobs_df)
+    st.pyplot(ax=barh_chart)
+
+with col[1]:
+    st.markdown('### Top 20 Needed Skills')
+
+    st.markdown('### Top 5 Need Skills by Level')
